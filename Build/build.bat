4a98d8171da2e8b61abe40e1ca7e0ac688154cea
Iliass Mahjoub <mahjoub.iliass@gmail.com>
2023-06-30 21:42:45 +0200

    initial commit

---
+++ Build/build.bat
@@ -0,0 +1,3 @@
+
+
+%CD%\..\tools\util\make\make.exe -f %CD%\..\Make\make_000.gmk all
---
+++ Src/Appli/Appli.c
@@ -0,0 +1,26 @@
+//#if !defined(_MSC_VER)
+// 0x40023830
+#define RCC_AHB1ENR *(volatile unsigned*) 0x40023830U
+
+// 0x40020000
+#define GPIOA_MODER *(volatile unsigned*) 0x40020000U
+
+#define GPIOA_ODR *(volatile unsigned*) 0x40020014U
+//#endif
+
+int main()
+{
+  RCC_AHB1ENR |= 1U;
+
+  GPIOA_MODER |= (1U << (5U * 2U));
+  GPIOA_MODER &= ~(1U << ((5U * 2U) + 1U));
+
+  for(;;)
+  {
+    GPIOA_ODR ^= (1U << 5); // toggle PortA.5
+
+    for(volatile unsigned count = 0U; count < 100000U; ++count) { ; }
+  }
+
+  return 0;
+}
