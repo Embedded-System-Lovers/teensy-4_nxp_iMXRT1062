e769d660b53d813e9ffe1a2ac024a75a264851e6
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-07-02 10:51:02 +0200

    Add bash shell script with execute permissions

---
+++ Build/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+
+SCRIPT_PATH=$(readlink -f "$BASH_SOURCE")
+SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
+
+build_command="make -f $SCRIPT_DIR/Make/make_000.gmk TYP_OS=unix $1"
+
+echo Executing Command: $build_command
+
+$build_command
+
+exit
