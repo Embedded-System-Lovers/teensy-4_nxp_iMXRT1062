d1289e6878937872ff6d2de25339b9c3cc3d2694
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-07-02 08:05:57 +0200

    Improve build and add Windows target build

--- Build/build.bat
+++ Build/build.bat
@@ -1,3 +1,7 @@
+echo off
 
+set BUILD_COMMAND=%CD%\..\tools\util\make\make.exe -f %CD%\..\Make\make_000.gmk %1%
 
-%CD%\..\tools\util\make\make.exe -f %CD%\..\Make\make_000.gmk all
+echo Build command is: %BUILD_COMMAND%
+
+%BUILD_COMMAND%
