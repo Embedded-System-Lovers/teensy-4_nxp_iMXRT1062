7f51116d9f47cf91b4f6e444d4446aa9b01e51b1
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-07-02 09:52:45 +0200

    Try for mixed win/ubuntu build in CI

--- Build/build.bat
+++ Build/build.bat
@@ -1,6 +1,6 @@
 echo off
 
-set BUILD_COMMAND=%CD%\..\tools\util\make\make.exe -f %CD%\..\Make\make_000.gmk %1%
+set BUILD_COMMAND=%CD%\..\tools\util\make\make.exe -f %CD%\..\Make\make_000.gmk TYP_OS=win %1%
 
 echo Build command is: %BUILD_COMMAND%
 
