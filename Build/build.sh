5661bac3547609f6ae58058d4bd9a862ae5bb518
Christopher Kormanyos <ckormanyos@yahoo.com>
2023-07-08 08:00:23 +0200

    Clean up Git query and unused symbols

--- Build/build.sh
+++ Build/build.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-
 SCRIPT_PATH=$(readlink -f "$BASH_SOURCE")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 
-(echo +++ Query git commit info; cd ../..; git log -n 1 --pretty=format:"Current Used Branch/Commit: - %d - %H"; echo)
+echo Query Git Commit:
+(cd ../..; git log -n 1 --pretty=format:"Current Used Branch/Commit: - %d - %H")
+echo
 
 build_command="make -f $SCRIPT_DIR/Make/make_000.gmk TYP_OS=unix $1"
 
